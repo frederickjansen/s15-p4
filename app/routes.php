c5d534495af8dae51a4f2a89547c482e79cd6bb8
Frederick Jansen <frederick.jansen@gmail.com>
2014-08-06 15:12:24 -0400

    Added all routes

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,28 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+// Index
+Route::get('/', 'PageController@index');
+
+// Login
+Route::get('/login', 'LoginController@getLogin');
+Route::post('/login', 'LoginController@postLogin');
+
+// About
+Route::get('/about', 'PageController@about');
+
+// Contact
+Route::get('/contact', 'PageController@getContact');
+Route::post('/contact', 'PageController@postContact');
+
+// Articles
+Route::get('/article', 'Article@index');
+Route::get('/article/create', 'Article@create');
+Route::post('/article', 'Article@store');
+Route::get('/article/{id}', 'Article@show');
+Route::get('/article/{id}/edit', 'Article@edit');
+Route::put('/article/{id}', 'Article@update');
+Route::delete('/article/{id}', 'Article@destroy');
+
+// Comments
+Route::post('/comment/{article_id}', 'Comment@postComment');
